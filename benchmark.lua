f5de99542b70200e8a56476d621aa57c94ba12f5
JaySixty <Jamie9192@me.com>
2024-06-04 01:10:27 +0000

    refactor

--- benchmark.lua
+++ benchmark.lua
@@ -1,19 +1,84 @@
-local iterations = 1000
+local iterations = 10000
 local how_many_times_for_average = 10
 
-local tests = {
-  {
-    name = "Joaat",
-    func = function()
-      joaat("weapon_pistol")
-    end
+local exampleTests = {
+
+  ["Hash"] = {
+    {
+      name = "Joaat",
+      func = function()
+        local wep = joaat("weapon_pistol")
+      end
+    },
+    {
+      name = "Hash",
+      func = function()
+        local wep = GetHashKey("weapon_pistol")
+      end
+    },
+    {
+      name = "Backticks (`)",
+      func = function()
+        local wep = `weapon_pistol`
+      end
+    }
   },
-  {
-    name = "Hash",
-    func = function()
-      GetHashKey("weapon_pistol")
-    end
-  }
+
+  ["Concat"] = {
+    {
+      name = "..",
+      func = function()
+        local str = "Hello"
+        str = str .. " World"
+      end
+    },
+    {
+      name = "string.format",
+      func = function()
+        local str = "Hello"
+        str = string.format("%s %s", str, "World")
+      end
+    },
+    {
+      name = "('%s'):format()",
+      func = function()
+        local str = "Hello"
+        str = ("%s %s"):format(str, "World")
+      end
+    },
+    {
+      name = "table.concat",
+      func = function()
+        local str = {"Hello"}
+        table.insert(str, "World")
+        table.concat(str, " ")
+      end
+    }
+  },
+
+  ["Insert"] = {
+    {
+      name = "table.insert",
+      func = function()
+        local tbl = {}
+        table.insert(tbl, i)
+      end
+    },
+    {
+      name = "Insert with pre-allocated size",
+      func = function()
+        local tbl = {}
+        tbl[1] = 1
+      end
+    },
+    {
+      name = "Insert with next available index",
+      func = function()
+        local tbl = {}
+        tbl[#tbl + 1] = i
+      end
+    }
+  },
 }
 
 ----------------------------------
@@ -32,17 +97,14 @@ function benchmarkF:add(data)
   assert(data.func, "Function is required")
   assert(type(data.name) == "string", "Name must be a string")
   assert(type(data.func) == "function", "Function must be a function")
-  self[data.name] = data.func
+  self.tests[data.name] = data.func
 end
 
-function benchmarkF:run()
+function benchmarkF:execute()
+  for name, func in pairs(self.tests) do
 
-  results = {}
-
-  for name, func in pairs(self) do
-
-    results[name] = results[name] or {}
-    results[name].times = {}
+    self.results[name] = self.results[name] or {}
+    self.results[name].times = {}
 
     for i = 1, how_many_times_for_average do
 
@@ -54,46 +116,68 @@ function benchmarkF:run()
 
       local time2 = os.nanotime()
 
-      table.insert(results[name].times, time2 - time)
+      table.insert(self.results[name].times, time2 - time)
 
     end
 
-    results[name].total = 0
+    self.results[name].total = 0
 
-    for i = 1, #results[name].times do
-      results[name].total += results[name].times[i]
+    for i = 1, #self.results[name].times do
+      self.results[name].total += self.results[name].times[i]
     end
 
-    results[name].average = results[name].total / #results[name].times
+    self.results[name].average = self:nanoToMilliseconds(self.results[name].total / #self.results[name].times)
   end
-
-  local str = ""
-
-  local fastest = nil
-
-  for name, data in pairs(results) do
-
-    str = ("%s%s: Time taken: %s\n"):format(str, name, data.average)
-
-    if not fastest or data.average < fastest.average then
-      fastest = data
-      fastest.name = name
-    end
-
-  end
-
-  str = ("%sIterations: %s. Checked %s times. Totalling: %s total iterations per function\n"):format(str, iterations, how_many_times_for_average, iterations * how_many_times_for_average)
-  str = str .. "------------------------------------\n"
-  str = ("%s\n\27[30m\27[42mFastest: %s \27[0m\n"):format(str, fastest.name)
-  str = str .. "------------------------------------\n"
-
-  print(str)
+  return self.results
 end
 
-local test = setmetatable({}, benchmarkF)
+function benchmarkF:nanoToMilliseconds(nano)
+  local secs = nano / 1000000
+  -- to 5 decimal places
+  return string.format("%.5f", secs)
+end
 
-for i = 1, #tests do
-  test:add(tests[i])
+function benchmarkF:print(results)
+  local leaderboard = {}
+  for name, data in pairs(self.results) do
+    table.insert(leaderboard, {name = name, data = data})
+  end
+
+  table.sort(leaderboard, function(a, b)
+    return a.data.average < b.data.average
+  end)
+
+  for i = 2, #leaderboard do
+    local diff = leaderboard[1].data.average - leaderboard[i].data.average
+    local percentage = diff / leaderboard[1].data.average * 100
+    leaderboard[i].data.percentage = math.abs(percentage)
+  end
+
+  print("Total tests: " .. #leaderboard .. ". Total iterations: " .. iterations .. ". Total average runs: " .. how_many_times_for_average .. ".")
+  print("Results:")
+  print("   1. " .. leaderboard[1].name .. " took an average of " .. leaderboard[1].data.average .. " milliseconds to run " .. iterations .. " times. ^2(Fastest)^7")
+  for i = 2, #leaderboard do
+    print("   " .. i .. ". " .. leaderboard[i].name .. " took an average of " .. leaderboard[i].data.average .. " milliseconds to run " .. iterations .. " times. (" .. leaderboard[i].data.percentage .. "% slower than the fastest)")
+  end
+end
+
+function benchmarkF:run()
+  self:execute()
+  self:print()
+end
+
+local test = setmetatable({
+  results = {},
+  tests = {},
+}, benchmarkF)
+
+local selectedTest = "Insert"
+if not exampleTests[selectedTest] then
+  print("Test not found")
+  return
+end
+for i = 1, #exampleTests[selectedTest] do
+  test:add(exampleTests[selectedTest][i])
 end
 
 test:run()
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Jay60'
+version '1.0.0'
+
+server_scripts{
+  "benchmark.lua"
+}
